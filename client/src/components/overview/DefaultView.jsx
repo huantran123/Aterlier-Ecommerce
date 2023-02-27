dd6b8185764507799909d632082b8aab26d318e1
Huan Tran <103070104+huantran123@users.noreply.github.com>
2023-02-26 23:46:07 -0800

    Update DefaultView.jsx

--- client/src/components/overview/DefaultView.jsx
+++ client/src/components/overview/DefaultView.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { HiArrowNarrowLeft, HiArrowNarrowRight } from 'react-icons/hi';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 class DefaultView extends React.Component {
   constructor(props) {
@@ -28,13 +26,11 @@ class DefaultView extends React.Component {
     return (
       <div className='default-view' role='big-picture'>
         <HiArrowNarrowLeft className={this.props.selectedPhotoIndex !== 0 ? 'arrow-icon' : 'arrow-icon invisible'} onClick={this.previous.bind(this)} />
-        {/* <FontAwesomeIcon icon={faArrowLeft} className={this.props.selectedPhotoIndex !== 0 ? 'arrow-icon' : 'arrow-icon hidden'} onClick={this.previous.bind(this)} /> */}
         {currentPhoto !== null
           ? <div className='big-picture magnify-cursor'style={{backgroundImage:`url(${currentPhoto})`}} onClick={this.props.toggleExpandedView} data-testid={currentPhoto} ></div>
           : <div className='big-picture magnify-cursor no-thumbnail' onClick={this.props.toggleExpandedView} ></div>
         }
         <HiArrowNarrowRight className={this.props.selectedPhotoIndex !== this.props.photos.length - 1 ? 'arrow-icon' : 'arrow-icon invisible'} onClick={this.next.bind(this)} />
-        {/* <FontAwesomeIcon icon={faArrowRight} className={this.props.selectedPhotoIndex !== this.props.photos.length - 1 ? 'arrow-icon' : 'arrow-icon hidden'} onClick={this.next.bind(this)} /> */}
       </div>
     )
   }
