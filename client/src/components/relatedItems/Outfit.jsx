61d41fb6d41c63615de254327df929dc3df261f4
Huan Tran <103070104+huantran123@users.noreply.github.com>
2023-02-26 23:09:11 -0800

    Update Outfit.jsx

--- client/src/components/relatedItems/Outfit.jsx
+++ client/src/components/relatedItems/Outfit.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import OutfitSlider from './OutfitSlider.jsx';
 import axios from 'axios';
 
-
 class Outfit extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       outfit: this.props.outfit
     }
-
   }
-
 
   componentDidUpdate(prevProps, prevState) {
     if(JSON.stringify(prevProps.outfit) !== JSON.stringify(this.props.outfit)) {
@@ -22,14 +18,11 @@ class Outfit extends React.Component {
     }
   }
 
-
-
   render() {
     return (
       <div className='widget'>
         <h4>Your Outfit</h4>
-          <OutfitSlider product={this.props.product} currentProduct={this.props.currentProduct} outfit={this.state.outfit} add={this.props.addToOutfit} delete={this.props.removeFromOutfit}/>
-          {/* <OutfitSlider product={this.props.product} outfit={this.state.curProduct} add={this.props.addToOutfit} delete={this.props.removeFromOutfit}/>  */}
+        <OutfitSlider product={this.props.product} currentProduct={this.props.currentProduct} outfit={this.state.outfit} add={this.props.addToOutfit} delete={this.props.removeFromOutfit}/>
       </div>
     )
   }
